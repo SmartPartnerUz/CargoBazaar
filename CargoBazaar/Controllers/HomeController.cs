e9c4ff128c15bffa2613f0510c257466592a52d6
Jahongir Ishankulov <ishankulovjahongir1507@gmail.com>
2023-05-31 17:11:50 +0500

    CODE RUB: Format Code

---
+++ CargoBazaar/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineer
+// --------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CargoBazaar.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+
+        public string Get() => "CargoBazar is running...";
+    }
+}
--- CargoBazaar/Program.cs
+++ CargoBazaar/Program.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineer
+// --------------------------------------------------------
+
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace CargoBazaar
 {
